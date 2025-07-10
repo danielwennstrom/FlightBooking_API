b2f1f51d3329da21a4de734618e3df4ff8a220c2
Daniel Wennström <daniel@wennstrom.io>
2025-07-10 12:37:28 +0200

    Install dotenv, Spring AI, set envars, etc

--- src/main/java/se/lexicon/flightbooking_api/FlightBookingApiApplication.java
+++ src/main/java/se/lexicon/flightbooking_api/FlightBookingApiApplication.java
@@ -1,12 +1,14 @@
 package se.lexicon.flightbooking_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightBookingApiApplication {
-
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().systemProperties().ignoreIfMissing().directory("src/main/java/se/lexicon/flightbooking_api/config/").load();
+        
         SpringApplication.run(FlightBookingApiApplication.class, args);
     }
 
