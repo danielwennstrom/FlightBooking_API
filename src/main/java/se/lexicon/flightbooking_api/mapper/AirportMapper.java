bb689f54d351efd9dce3088c9cf6ed546fff3ed0
Daniel Wennström <daniel@wennstrom.io>
2025-07-30 15:22:01 +0200

    AirportMapper uses MapStruct

--- src/main/java/se/lexicon/flightbooking_api/mapper/AirportMapper.java
+++ src/main/java/se/lexicon/flightbooking_api/mapper/AirportMapper.java
@@ -1,5 +1,6 @@
 package se.lexicon.flightbooking_api.mapper;
 
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 import se.lexicon.flightbooking_api.dto.AirportDTO;
 import se.lexicon.flightbooking_api.entity.Airport;
@@ -7,43 +8,10 @@ import se.lexicon.flightbooking_api.entity.Airport;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
-public class AirportMapper {
-    public AirportDTO toDTO(Airport airport) {
-        if (airport == null) return null;
-        return new AirportDTO(
-                airport.getIataCode(),
-                airport.getIcaoCode(),
-                airport.getName(),
-                airport.getIsoCountry(),
-                airport.getCountryName(),
-                airport.getMunicipality(),
-                airport.getDisplayName()
-        );
-    }
-
-    public Airport toEntity(AirportDTO dto) {
-        if (dto == null) return null;
-        Airport airport = new Airport();
-        airport.setIataCode(dto.getIataCode());
-        airport.setIcaoCode(dto.getIcaoCode());
-        airport.setName(dto.getName());
-        airport.setIsoCountry(dto.getIsoCountry());
-        airport.setMunicipality(dto.getMunicipality());
-        return airport;
-    }
-
-    public List<AirportDTO> toDTOList(List<Airport> airports) {
-        if (airports == null) return List.of();
-        return airports.stream()
-                .map(this::toDTO)
-                .collect(Collectors.toList());
-    }
-
-    public List<Airport> toEntityList(List<AirportDTO> dtos) {
-        if (dtos == null) return List.of();
-        return dtos.stream()
-                .map(this::toEntity)
-                .collect(Collectors.toList());
-    }
+@Mapper(componentModel = "spring")
+public interface AirportMapper {
+    Airport toDto(AirportDTO dto);
+    AirportDTO toEntity(Airport entity);
+    List<AirportDTO> toDTOList(List<Airport> airports);
+    List<Airport> toEntityList(List<AirportDTO> dtos);
 }
